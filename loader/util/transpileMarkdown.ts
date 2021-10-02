2b1069486da4248a99bb4eade495e59c38bb1c17
Kei Ito <kei.itof@gmail.com>
2021-10-03 08:17:05 +0900

    feat: transpileMarkdown exposes Markdown.Root

--- loader/util/transpileMarkdown.ts
+++ loader/util/transpileMarkdown.ts
@@ -1,10 +1,20 @@
-import {serializeMarkdownToJsx} from '../serialize/MarkdownToJsx';
+import type Markdown from 'mdast';
+import {serializeMarkdownRootToJsx} from '../serialize/MarkdownToJsx';
 import {listImportDeclarations} from './listImportDeclarations';
 import {createSerializeMarkdownContext} from './createSerializeMarkdownContext';
 
-export const transpileMarkdown = async (source: string) => {
+export interface TranspileMarkdownResult {
+    jsx: string,
+    imports: string,
+    root: Markdown.Root,
+}
+
+export const transpileMarkdown = async (
+    source: string,
+): Promise<TranspileMarkdownResult> => {
     const context = await createSerializeMarkdownContext();
-    const jsx = [...serializeMarkdownToJsx(context, source)].join('');
+    const root = context.fromMarkdown(source);
+    const jsx = [...serializeMarkdownRootToJsx(context, root)].join('');
     const imports = [...listImportDeclarations(context)].join('\n');
-    return {jsx, imports};
+    return {jsx, imports, root};
 };
