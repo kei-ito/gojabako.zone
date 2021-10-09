b662b12c250075338b6cf2b8452ba9690c318988
Kei Ito <kei.itof@gmail.com>
2021-10-09 10:30:51 +0900

    refactor: use top level

--- src-tool/build/atom.ts
+++ src-tool/build/atom.ts
@@ -1,19 +1,11 @@
 import * as fs from 'fs';
-import * as console from 'console';
 import {publicUrl} from '../util/url';
 import {serializeAtom} from '../serialize/Atom';
 
-const buildAtom = async () => {
-    const writer = fs.createWriteStream(
-        new URL('feed.atom', publicUrl),
-    );
-    for await (const line of serializeAtom()) {
-        writer.write(`${line}\n`);
-    }
-    writer.end();
-};
-
-buildAtom().catch((error: unknown) => {
-    console.error(error);
-    process.exit(1);
-});
+const writer = fs.createWriteStream(
+    new URL('feed.atom', publicUrl),
+);
+for await (const line of serializeAtom()) {
+    writer.write(`${line}\n`);
+}
+writer.end();
--- src-tool/build/sitemap.ts
+++ src-tool/build/sitemap.ts
@@ -1,19 +1,11 @@
 import * as fs from 'fs';
-import * as console from 'console';
 import {publicUrl} from '../util/url';
 import {serializeSitemap} from '../serialize/Sitemap';
 
-const buildSitemap = async () => {
-    const writer = fs.createWriteStream(
-        new URL('sitemap.xml', publicUrl),
-    );
-    for await (const line of serializeSitemap()) {
-        writer.write(`${line}\n`);
-    }
-    writer.end();
-};
-
-buildSitemap().catch((error: unknown) => {
-    console.error(error);
-    process.exit(1);
-});
+const writer = fs.createWriteStream(
+    new URL('sitemap.xml', publicUrl),
+);
+for await (const line of serializeSitemap()) {
+    writer.write(`${line}\n`);
+}
+writer.end();
