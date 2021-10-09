fd6ba686bf86152d50a4a8f11665143baaa39d66
Kei Ito <kei.itof@gmail.com>
2021-10-09 20:20:52 +0900

    fix: test for getFileData

--- src-tool/util/getFileData.test.ts
+++ src-tool/util/getFileData.test.ts
@@ -1,13 +1,14 @@
-import {Date} from '../../src/global';
 import {getFileData} from './getFileData';
 import {projectRootUrl} from './url';
+
+const DateRegExp = /\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d\d\dZ/;
 
 describe(getFileData.name, () => {
     it('package.json', async () => {
         const fileUrl = new URL('package.json', projectRootUrl);
         const {firstCommitAt, lastCommitAt} = await getFileData(fileUrl);
-        expect(firstCommitAt).toBeInstanceOf(Date);
-        expect(lastCommitAt).toBeInstanceOf(Date);
+        expect(firstCommitAt).toMatch(DateRegExp);
+        expect(lastCommitAt).toMatch(DateRegExp);
     });
     it('.git', async () => {
         const fileUrl = new URL('.git', projectRootUrl);
