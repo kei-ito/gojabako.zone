11404627739c77d7a859b4020ac4005688f94118
Kei Ito <kei.itof@gmail.com>
2021-10-10 10:55:23 +0900

    refactor: make exec() quiet

--- src-tool/util/exec.ts
+++ src-tool/util/exec.ts
@@ -10,7 +10,6 @@ export const exec = async (
     command: string,
     options: childProcess.ExecOptions = {},
 ): Promise<ExecResult> => await new Promise<ExecResult>((resolve, reject) => {
-    console.info(command);
     childProcess.exec(command, options, (error, rawStdout, rawStderr) => {
         if (error) {
             reject(error);
@@ -18,7 +17,9 @@ export const exec = async (
             const stdout = rawStdout.trim();
             const stderr = rawStderr.trim();
             if (stderr) {
-                console.error(stderr);
+                console.info(`--- command --------\n${command}`);
+                console.info(`--- stdout --------\n${stdout}`);
+                console.error(`--- stderr --------\n${stderr}`);
             }
             resolve({stdout, stderr});
         }
