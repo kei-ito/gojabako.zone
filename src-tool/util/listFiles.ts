bab7b9f80a8ddbff7825203197302c2424df60d2
Kei Ito <kei.itof@gmail.com>
2021-10-13 09:02:56 +0900

    fix: trailing slash

--- src-tool/util/listFiles.ts
+++ src-tool/util/listFiles.ts
@@ -2,10 +2,10 @@ import * as fs from 'fs';
 
 export const listFiles = async function* (directoryUrl: URL): AsyncGenerator<URL> {
     for await (const name of await fs.promises.readdir(directoryUrl as unknown as string)) {
-        const fileUrl = new URL(`${name}/`, directoryUrl);
+        const fileUrl = new URL(name, directoryUrl);
         const stats = await fs.promises.stat(fileUrl);
         if (stats.isDirectory()) {
-            yield* listFiles(fileUrl);
+            yield* listFiles(new URL(`${name}/`, directoryUrl));
         } else if (stats.isFile()) {
             yield fileUrl;
         }
