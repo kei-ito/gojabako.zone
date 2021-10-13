075fe34fb18d45bfc636f65e01d3ef385fc861c9
Kei Ito <kei.itof@gmail.com>
2021-10-13 09:00:16 +0900

    refactor: Using opendir like 78a0c31adc640b26e19416d327da70ca72634195 can be a problem if the tree is deep, as it leaves many resources open.

--- src-tool/util/listFiles.ts
+++ src-tool/util/listFiles.ts
@@ -1,13 +1,13 @@
 import * as fs from 'fs';
 
-export const listFiles = async function* (
-    directoryUrl: URL,
-): AsyncGenerator<URL> {
-    for await (const dirEnt of await fs.promises.opendir(directoryUrl as unknown as string)) {
-        if (dirEnt.isDirectory()) {
-            yield* listFiles(new URL(`${dirEnt.name}/`, directoryUrl));
-        } else if (dirEnt.isFile()) {
-            yield new URL(dirEnt.name, directoryUrl);
+export const listFiles = async function* (directoryUrl: URL): AsyncGenerator<URL> {
+    for await (const name of await fs.promises.readdir(directoryUrl as unknown as string)) {
+        const fileUrl = new URL(`${name}/`, directoryUrl);
+        const stats = await fs.promises.stat(fileUrl);
+        if (stats.isDirectory()) {
+            yield* listFiles(fileUrl);
+        } else if (stats.isFile()) {
+            yield fileUrl;
         }
     }
 };
