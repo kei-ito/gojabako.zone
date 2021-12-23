812d575372f3f561026578a61809b87eb18f15fd
Kei Ito <kei.itof@gmail.com>
2021-12-23 09:42:33 +0900

    chore: pageListの更新

--- src/pageList.ts
+++ src/pageList.ts
@@ -10,7 +10,7 @@ export interface PageData {
     commitCount: number,
 }
 export const pageListByPublishedAt: Array<PageData> = JSON.parse(`[
-{"pathname":"/2021/aws-errors/","title":"AWSの開発で見たエラーと原因","filePath":"src/pages/2021/aws-errors/index.page.md","publishedAt":"2021-12-17T00:15:30+09:00","updatedAt":"2021-12-17T00:15:30+09:00","commitCount":1},
+{"pathname":"/2021/aws-errors/","title":"AWSの開発で見たエラーと原因","filePath":"src/pages/2021/aws-errors/index.page.md","publishedAt":"2021-12-17T00:15:30+09:00","updatedAt":"2021-12-23T09:42:07+09:00","commitCount":2},
 {"pathname":"/2021/ipad-case/","title":"iPad Proケースの製作","filePath":"src/pages/2021/ipad-case/index.page.md","publishedAt":"2021-12-13T22:53:16+09:00","updatedAt":"2021-12-14T01:16:34+09:00","commitCount":2},
 {"pathname":"/2021/au-denki/","title":"auでんきの使用量をCSVにする","filePath":"src/pages/2021/au-denki/index.page.md","publishedAt":"2021-12-01T00:59:21+09:00","updatedAt":"2021-12-01T01:30:59+09:00","commitCount":4},
 {"pathname":"/2021/nextjs12/","title":"Next.js 12にしました","filePath":"src/pages/2021/nextjs12/index.page.md","publishedAt":"2021-10-29T22:24:14+09:00","updatedAt":"2021-11-25T12:56:34+09:00","commitCount":4},
