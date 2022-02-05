4a447454f8b2d389499ffae313c0a016caff4e9f
Kei Ito <kei.itof@gmail.com>
2022-02-05 13:14:23 +0900

    fix: pageList

--- packages/page/getPageList.ts
+++ packages/page/getPageList.ts
@@ -5,15 +5,9 @@ import {findPageData} from './getPageData';
 import {Date, Error, JSON, WeakMap} from '../es/global';
 import {rootDirectoryPath} from '../fs/constants';
 
-const isPageFile = (filePath: string) => {
-    if (filePath.endsWith('.component.tsx')) {
-        return false;
-    }
-    if (filePath.endsWith('.page.md')) {
-        return true;
-    }
-    return (/\.tsx?$/).test(filePath);
-};
+const isPageFile = (filePath: string) => filePath.endsWith('.page.tsx')
+|| filePath.endsWith('.page.ts')
+|| filePath.endsWith('.page.md');
 const byDate = (date1: string, date2: string) => {
     const now = Date.now();
     const t1 = date1 ? new Date(date1).getTime() : now;
@@ -22,14 +16,21 @@ const byDate = (date1: string, date2: string) => {
 };
 const byPublishedAt = (page1: PageData, page2: PageData) => byDate(page1.publishedAt, page2.publishedAt);
 const byUpdatedAt = (page1: PageData, page2: PageData) => byDate(page1.updatedAt, page2.updatedAt);
+const listPageData = async function* () {
+    for await (const fileUrl of listFiles(path.join(rootDirectoryPath, 'src/pages'))) {
+        if (isPageFile(fileUrl)) {
+            const pageData = await findPageData(fileUrl);
+            if (pageData) {
+                yield pageData;
+            }
+        }
+    }
+};
 
 export const getPageList = async () => {
     const pageList: Array<PageData> = [];
-    for await (const fileUrl of listFiles(path.join(rootDirectoryPath, 'src/pages'), isPageFile)) {
-        const pageData = await findPageData(fileUrl);
-        if (pageData) {
-            pageList.push(pageData);
-        }
+    for await (const pageData of listPageData()) {
+        pageList.push(pageData);
     }
     const pageListByPublishedAt = pageList.slice().sort(byPublishedAt);
     const indexMapping = new WeakMap(pageListByPublishedAt.map((page, index) => [page, index]));
