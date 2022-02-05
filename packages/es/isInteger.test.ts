5f6ef28b8f314963a55180e351217d2c9f6b293a
Kei Ito <kei.itof@gmail.com>
2022-02-05 12:37:45 +0900

    test: fix tests

--- packages/es/isInteger.test.ts
+++ packages/es/isInteger.test.ts
@@ -20,18 +20,18 @@ describe(isInteger.name, () => {
 
 describe(isPositiveInteger.name, () => {
     it('test values', () => {
-        expect(isInteger('a')).toBe(false);
-        expect(isInteger(null)).toBe(false);
-        expect(isInteger(undefined)).toBe(false);
-        expect(isInteger(NaN)).toBe(false);
-        expect(isInteger(Infinity)).toBe(false);
-        expect(isInteger(false)).toBe(false);
-        expect(isInteger(() => null)).toBe(false);
-        expect(isInteger({})).toBe(false);
-        expect(isInteger([])).toBe(false);
-        expect(isInteger(0)).toBe(false);
-        expect(isInteger(1)).toBe(true);
-        expect(isInteger(-2)).toBe(false);
-        expect(isInteger(1.4)).toBe(false);
+        expect(isPositiveInteger('a')).toBe(false);
+        expect(isPositiveInteger(null)).toBe(false);
+        expect(isPositiveInteger(undefined)).toBe(false);
+        expect(isPositiveInteger(NaN)).toBe(false);
+        expect(isPositiveInteger(Infinity)).toBe(false);
+        expect(isPositiveInteger(false)).toBe(false);
+        expect(isPositiveInteger(() => null)).toBe(false);
+        expect(isPositiveInteger({})).toBe(false);
+        expect(isPositiveInteger([])).toBe(false);
+        expect(isPositiveInteger(0)).toBe(false);
+        expect(isPositiveInteger(1)).toBe(true);
+        expect(isPositiveInteger(-2)).toBe(false);
+        expect(isPositiveInteger(1.4)).toBe(false);
     });
 });
