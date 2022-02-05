d6e6d20adb02ed58dcbe9d2374a82d7de97e3570
Kei Ito <kei.itof@gmail.com>
2022-02-05 10:09:59 +0900

    feat: use preprocessed images

--- loader/page.md.cjs
+++ loader/page.md.cjs
@@ -7,5 +7,6 @@
 module.exports = async function markdownPageLoader(source) {
     const {loadMarkdownPage} = await import('../.output/loader/page.md.mjs');
     const code = await loadMarkdownPage(this, source);
+    // console.info(code);
     return code;
 };
