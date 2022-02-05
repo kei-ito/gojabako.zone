6757f30149444cdde23825b532f0aa912334524f
Kei Ito <kei.itof@gmail.com>
2022-02-05 13:34:24 +0900

    fix: pageList

--- packages/node/getPagePathName.ts
+++ packages/node/getPagePathName.ts
@@ -1,5 +1,4 @@
 import * as path from 'path';
-import {getExtension} from '../es/getExtension';
 import {getBaseName} from '../es/getBaseName';
 import {rootDirectoryPath} from '../fs/constants';
 import {Error} from '../es/global';
@@ -7,27 +6,15 @@ import {Error} from '../es/global';
 const pagesDirectoryPath = path.join(rootDirectoryPath, 'src/pages');
 
 export const getPagePathName = (fileAbsolutePath: string): string | null => {
+    const basename = getBaseName(fileAbsolutePath);
+    if (basename.startsWith('_')) {
+        return null;
+    }
     const normalizedFileAbsolutePath = path.normalize(fileAbsolutePath);
     if (!normalizedFileAbsolutePath.startsWith(pagesDirectoryPath)) {
         throw new Error(`The page file isn't in the pages directory: ${normalizedFileAbsolutePath}`);
     }
-    let pathname = ['', ...path.relative(pagesDirectoryPath, normalizedFileAbsolutePath).split(path.sep)].join('/');
-    if (pathname.endsWith('.page.md')) {
-        pathname = pathname.slice(0, -8);
-    } else {
-        const basename = getBaseName(pathname);
-        if (basename.startsWith('_')) {
-            return null;
-        }
-        const extname = getExtension(pathname);
-        switch (extname) {
-        case '.tsx':
-        case '.jsx':
-            pathname = pathname.slice(0, -extname.length);
-            break;
-        default:
-            return null;
-        }
-    }
-    return pathname.replace(/(\/?)index$/, '/');
+    return ['', ...path.relative(pagesDirectoryPath, normalizedFileAbsolutePath).split(path.sep)].join('/')
+    .replace(/\.page\.\w+$/, '')
+    .replace(/(\/?)index$/, '/');
 };
