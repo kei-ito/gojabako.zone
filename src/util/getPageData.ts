4a9bd7124df6b8d897b14f658ff778c72f43a5f8
Kei Ito <kei.itof@gmail.com>
2022-02-05 19:52:26 +0900

    fix: ignore trailing slash

--- src/util/getPageData.ts
+++ src/util/getPageData.ts
@@ -3,10 +3,11 @@ import type {PageData} from '../pageList';
 import {pageListByPublishedAt} from '../pageList';
 
 export const getPageData = (pathname: string): PageData => {
+    const normalized = pathname.replace(/\/$/, '');
     for (const page of pageListByPublishedAt) {
-        if (page.pathname === pathname) {
+        if (page.pathname === normalized) {
             return page;
         }
     }
-    throw new Error(`NoSuchPage: ${pathname}`);
+    throw new Error(`NoSuchPage: ${pathname} (${normalized})`);
 };
