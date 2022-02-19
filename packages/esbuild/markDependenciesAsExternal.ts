2d6bea3842adf7798b9edaf34764d9488b97485d
Kei Ito <kei.itof@gmail.com>
2022-02-19 21:46:48 +0900

    fix: prepare externalModuleNames

--- packages/esbuild/markDependenciesAsExternal.ts
+++ packages/esbuild/markDependenciesAsExternal.ts
@@ -1,32 +1,36 @@
 import type * as esbuild from 'esbuild';
 import {builtinModules} from 'module';
-import {Error} from '../es/global';
+import {Error, JSON, Object, Set} from '../es/global';
 import {packageJson} from '../fs/constants';
 
 export const markDependenciesAsExternal = (
-    {includeDev}: {includeDev?: boolean} = {},
-): esbuild.Plugin => ({
-    name: 'markDependenciesAsExternal',
-    setup: (build) => {
-        build.onResolve({filter: /^[^.]/}, ({path, importer}) => {
-            if (!importer) {
-                return null;
-            }
-            let slashIndex = path.indexOf('/');
-            if (slashIndex < 0) {
-                slashIndex = path.length;
-            }
-            const moduleName = path.slice(0, slashIndex);
-            if (builtinModules.includes(moduleName)) {
-                return {path, external: true};
-            }
-            if (includeDev && (moduleName in packageJson.devDependencies)) {
-                return {path, external: true};
-            }
-            if (moduleName in packageJson.dependencies) {
-                return {path, external: true};
-            }
-            throw new Error(`${importer} imports ${path} (${moduleName}) but dependencies doesn't have it.`);
-        });
-    },
-});
+    {includeDev}: {includeDev?: boolean} = {includeDev: false},
+): esbuild.Plugin => {
+    const externalModuleNames = new Set([
+        ...builtinModules,
+        ...Object.keys(packageJson.dependencies),
+        ...(includeDev ? Object.keys(packageJson.devDependencies) : []),
+    ]);
+    return {
+        name: 'markDependenciesAsExternal',
+        setup: (build) => {
+            build.onResolve({filter: /^[^.]/}, ({path, importer}) => {
+                if (!importer) {
+                    return null;
+                }
+                if (externalModuleNames.has(path)) {
+                    return {path, external: true};
+                }
+                let slashIndex = path.indexOf('/');
+                if (slashIndex < 0) {
+                    slashIndex = path.length;
+                }
+                const moduleName = path.slice(0, slashIndex);
+                if (externalModuleNames.has(moduleName)) {
+                    return {path, external: true};
+                }
+                throw new Error(`${importer} imports ${path} (${moduleName}) but dependencies doesn't have it: ${JSON.stringify([...externalModuleNames], null, 2)}`);
+            });
+        },
+    };
+};
