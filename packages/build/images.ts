33201800ff84fff019c73a608a2919d78bb329b5
Kei Ito <kei.itof@gmail.com>
2022-02-20 23:32:59 +0900

    build: add messages

--- packages/build/images.ts
+++ packages/build/images.ts
@@ -97,22 +97,26 @@ const loadPreviousResult = async (outputDirectory: string) => {
     const resultPath = path.join(outputDirectory, resultFileName);
     const json = await fs.promises.readFile(resultPath, 'utf8').catch(ignoreENOENT);
     if (!json) {
+        console.info(`NoResult: ${outputDirectory}`);
         return null;
     }
     const parsed: unknown = JSON.parse(json);
     if (isEncodeResult(parsed)) {
         if (parsed.version !== version) {
+            console.info(`OldVersion: ${JSON.stringify(parsed.version, null, 4)}`);
             return null;
         }
         for (const result of parsed.results) {
             const filePath = path.join(publicDirectory, ...result.path.split('/'));
             const stats = await fs.promises.stat(filePath).catch(ignoreENOENT);
             if (!stats || !stats.isFile() || stats.size !== result.size) {
+                console.info(`FileChanged: ${JSON.stringify(result, null, 4)}`);
                 return null;
             }
         }
         return parsed;
     }
+    console.info(`UnknownFormat: ${JSON.stringify(parsed, null, 4)}`);
     return null;
 };
 
