87ed741dfe704d23c42797822f0e814421e8b581
Kei Ito <kei.itof@gmail.com>
2022-02-23 20:27:05 +0900

    chore: generate page images

--- src/pageImageList.ts
+++ src/pageImageList.ts
@@ -7,82 +7,82 @@ export interface PageImageData {
 }
 export const pageImages: Record<string, PageImageData | undefined> = {
     "/2018/monitor-stand": {
-        "path": "/covers/M6w8jGfI/ySum7RbI.png",
+        "path": "/covers/M6w8jGfI/C2-Y8cjd.png",
         "width": 1200,
         "height": 630
     },
     "/2019/error-propagation": {
-        "path": "/covers/SWD-1E4N/ybM5adoe.png",
+        "path": "/covers/SWD-1E4N/ZlUDNA7P.png",
         "width": 1200,
         "height": 630
     },
     "/2019/gitlab-runner": {
-        "path": "/covers/TmSvdGod/vlrVX1vL.png",
+        "path": "/covers/TmSvdGod/4dQrixIl.png",
         "width": 1200,
         "height": 630
     },
     "/2020/new-computer": {
-        "path": "/covers/-AL1gWae/ar81kn3l.png",
+        "path": "/covers/-AL1gWae/trCiKMhT.png",
         "width": 1200,
         "height": 630
     },
     "/2021/au-denki": {
-        "path": "/covers/9cPn0k2Z/JQEZL4tL.png",
+        "path": "/covers/9cPn0k2Z/eF0jBzyy.png",
         "width": 1200,
         "height": 630
     },
     "/2021/aws-errors": {
-        "path": "/covers/TzRZIKCA/g7c2Y1Ws.png",
+        "path": "/covers/TzRZIKCA/itd5oMYi.png",
         "width": 1200,
         "height": 630
     },
     "/2021/ipad-case": {
-        "path": "/covers/3pChiagg/l8FMXNGk.png",
+        "path": "/covers/3pChiagg/SAnCUVLF.png",
         "width": 1200,
         "height": 630
     },
     "/2021/nextjs": {
-        "path": "/covers/s373Le5-/66-uBoo_.png",
+        "path": "/covers/s373Le5-/f5C1CLpb.png",
         "width": 1200,
         "height": 630
     },
     "/2021/nextjs-https": {
-        "path": "/covers/ZX-cXk49/8pG-mYBl.png",
+        "path": "/covers/ZX-cXk49/UUYC66Kj.png",
         "width": 1200,
         "height": 630
     },
     "/2021/nextjs12": {
-        "path": "/covers/FqWvXwk8/uCSFb_bN.png",
+        "path": "/covers/FqWvXwk8/CF44ti-s.png",
         "width": 1200,
         "height": 630
     },
     "/2022/image-preprocessor": {
-        "path": "/covers/foR4ezoK/dubp6ROu.png",
+        "path": "/covers/foR4ezoK/dZLfIiR5.png",
         "width": 1200,
         "height": 630
     },
     "/2022/iphone12": {
-        "path": "/covers/C4al8FNS/0oJXs0N9.png",
+        "path": "/covers/C4al8FNS/aWTDW8Id.png",
         "width": 1200,
         "height": 630
     },
     "/2022/page-images": {
-        "path": "/covers/yEbsLghZ/jQ8f-j4y.png",
+        "path": "/covers/yEbsLghZ/jThS4d0F.png",
         "width": 1200,
         "height": 630
     },
     "/author": {
-        "path": "/covers/2NbUOuVG/GgcTDfDv.png",
+        "path": "/covers/2NbUOuVG/j3Zz3iXi.png",
         "width": 1200,
         "height": 630
     },
     "": {
-        "path": "/covers/47DEQpj8/3ZzHTD6r.png",
+        "path": "/covers/47DEQpj8/9lI8pT2M.png",
         "width": 1200,
         "height": 630
     },
     "/markdown": {
-        "path": "/covers/tiihd--T/WeNAiShp.png",
+        "path": "/covers/tiihd--T/ddbr1hkJ.png",
         "width": 1200,
         "height": 630
     }
