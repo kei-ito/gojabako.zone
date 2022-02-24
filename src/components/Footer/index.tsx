34e81f3ea69154e6cc986cade54b8743b0f5c9f5
Kei Ito <kei.itof@gmail.com>
2022-02-24 18:05:31 +0900

    fix: detailsの閉じる機能は使わない

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,12 +6,12 @@ import {className} from './style.module.css';
 export const Footer = () => <footer className={className.footer}>
     <section>
         <h2>書いたもの</h2>
-        {categorizedPageListByPublishedAt.blogPost.map(([year, pages]) => <details key={year} open={true}>
-            <summary><p>{year}</p></summary>
+        {categorizedPageListByPublishedAt.blogPost.map(([year, pages]) => <div key={year}>
+            <p>{year}</p>
             <ul>
                 {pages.map((page) => <li key={page.pathname}><PageLinkPublished {...page}/></li>)}
             </ul>
-        </details>)}
+        </div>)}
         <h2>その他</h2>
         <ul>{categorizedPageListByPublishedAt.others.map((page) => <li key={page.pathname}><PageLinkPublished {...page}/></li>)}</ul>
     </section>
