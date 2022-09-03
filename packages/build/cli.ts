4086e6d2102cc3c3ee71baae382ddff3b14b831b
Kei Ito <kei.itof@gmail.com>
2022-09-03 18:07:51 +0900

    refactor: delete runScript

--- packages/build/cli.ts
+++ packages/build/cli.ts
@@ -4,21 +4,18 @@ import * as path from 'path';
 import * as esbuild from 'esbuild';
 import {rootDirectory} from '../../paths.mjs';
 import {markDependenciesAsExternal} from '../esbuild/markDependenciesAsExternal';
-import {runScript} from '../node/runScript';
 
-runScript(async () => {
-    const buildDirectoryPath = path.join(rootDirectory, 'packages/build');
-    for (const name of await fs.promises.readdir(buildDirectoryPath)) {
-        if (name !== 'cli.ts' && name.endsWith('.ts')) {
-            await esbuild.build({
-                entryPoints: [path.join(buildDirectoryPath, name)],
-                outfile: path.join(rootDirectory, `.output/build/${name.slice(0, -3)}.mjs`),
-                plugins: [markDependenciesAsExternal({includeDev: true})],
-                bundle: true,
-                target: 'esnext',
-                format: 'esm',
-            });
-            console.info(`BuildCLI: ${name}`);
-        }
+const buildDirectoryPath = path.join(rootDirectory, 'packages/build');
+for (const name of await fs.promises.readdir(buildDirectoryPath)) {
+    if (name !== 'cli.ts' && name.endsWith('.ts')) {
+        await esbuild.build({
+            entryPoints: [path.join(buildDirectoryPath, name)],
+            outfile: path.join(rootDirectory, `.output/build/${name.slice(0, -3)}.mjs`),
+            plugins: [markDependenciesAsExternal({includeDev: true})],
+            bundle: true,
+            target: 'esnext',
+            format: 'esm',
+        });
+        console.info(`BuildCLI: ${name}`);
     }
-});
+}
