cf8624752b1665920f86bf5f20de9a75ca98f07e
Kei Ito <kei.itof@gmail.com>
2022-09-03 16:33:24 +0900

    chore: delete unused modules

--- packages/fs/constants.ts
+++ packages/fs/constants.ts
@@ -5,7 +5,6 @@ import {JSON} from '../es/global';
 
 export const rootDirectoryPath = process.cwd();
 export const publicDirectory = path.join(rootDirectoryPath, 'public');
-export const processedImagesDirectory = path.join(publicDirectory, 'images');
 export const coverImagesDirectory = path.join(publicDirectory, 'covers');
 export const packageJson = JSON.parse(
     fs.readFileSync(path.join(rootDirectoryPath, 'package.json'), 'utf-8'),
