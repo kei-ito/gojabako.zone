6c872858d67fd3746d04a0bba98fecd3b7f39a92
Kei Ito <kei.itof@gmail.com>
2023-09-16 17:46:27 +0900

    feat: add icons for apps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/google-font-display, @next/next/no-page-custom-font */
 import './globals.scss';
 import type { PropsWithChildren } from 'react';
 import { SiteFooter } from '../components/SiteFooter';
@@ -29,6 +30,10 @@ export default function RootLayout({ children }: PropsWithChildren) {
           href="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css"
           integrity="sha384-GvrOXuhMATgEsSwCs4smul74iXGOixntILdUW9XmUC6+HX0sLNAK3q71HotJqlAn"
           crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          rel="stylesheet"
         />
         <meta name="theme-color" content="hsla(0,0%,100%,0.8)" />
       </head>
