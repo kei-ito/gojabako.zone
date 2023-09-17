626528e8a2c48ac5629acba5163594c1c171e0af
Kei Ito <kei.itof@gmail.com>
2023-09-17 18:44:59 +0900

    chore: delete old scripts

--- old-src/util/isBrowser.ts
+++
@@ -1,1 +0,0 @@
-export const isBrowser = 'window' in globalThis;
--- old-src/util/isHTMLElement.ts
+++
@@ -1,3 +0,0 @@
-export const isHTMLElement = (
-  node: Element | Node | null,
-): node is HTMLElement => Boolean(node && node.nodeType === node.ELEMENT_NODE);
