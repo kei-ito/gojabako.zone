ccc0358f7bb01701836498b8844a72051bd8a8fe
Kei Ito <kei.itof@gmail.com>
2023-09-21 12:56:32 +0900

    fix: delimiter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { site } from '../util/site.mts';
 
 export const metadata: Metadata = {
   metadataBase: site.baseUrl,
-  title: { template: `%s ・ ${site.name}`, default: site.name },
+  title: { template: `%s | ${site.name}`, default: site.name },
   applicationName: site.name,
   themeColor: 'hsla(0,0%,100%,0.8)',
   authors: [site.author],
