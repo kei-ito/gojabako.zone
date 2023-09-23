3c0ca24e8689a2371be64e3f8ed1160ae6591e15
Kei Ito <kei.itof@gmail.com>
2023-09-23 09:58:16 +0900

    feat: ButtonProps.stateを追加

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,10 +6,15 @@ import type {
 import { classnames } from '../../util/classnames.mts';
 import * as style from './style.module.scss';
 
-export const Button = (
-  props: PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>,
-) => (
-  <button {...props} className={classnames(style.button, props.className)} />
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  state?: 'active' | 'focus' | 'hover';
+}
+
+export const Button = ({ state, ...props }: PropsWithChildren<ButtonProps>) => (
+  <button
+    {...props}
+    className={classnames(style.button, state && style[state], props.className)}
+  />
 );
 
 export const Buttons = (
