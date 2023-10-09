c089a308b00119f0fecfc0845185a4fe4bd3f15f
Kei Ito <kei.itof@gmail.com>
2023-10-10 00:01:37 +0900

    fix: 初期化処理をRecoilRootに追加

--- src/components/DistributedReversi/index.tsx
+++ src/components/DistributedReversi/index.tsx
@@ -1,13 +1,18 @@
 'use client';
 import { useSearchParams } from 'next/navigation';
 import type { HTMLAttributes } from 'react';
+import { useCallback } from 'react';
+import type { MutableSnapshot } from 'recoil';
 import { RecoilRoot } from 'recoil';
 import { classnames } from '../../util/classnames.mts';
 import { getCurrentUrl } from '../../util/getCurrentUrl.mts';
 import { DRBoard } from './Board';
 import { DRFloater } from './Floater';
 import { DRInfo } from './Info';
+import { rcAddCells } from './recoil.app.mts';
 import * as style from './style.module.scss';
+import type { DRCellId } from './util.mts';
+import { toDRCellId } from './util.mts';
 
 export const DistributedReversi = (props: HTMLAttributes<HTMLElement>) => {
   getCurrentUrl.defaultSearchParams = useSearchParams();
@@ -16,7 +21,7 @@ export const DistributedReversi = (props: HTMLAttributes<HTMLElement>) => {
       {...props}
       className={classnames(style.container, props.className)}
     >
-      <RecoilRoot>
+      <RecoilRoot initializeState={useInit()}>
         <DRBoard />
         <DRInfo />
         <DRFloater />
@@ -24,3 +29,15 @@ export const DistributedReversi = (props: HTMLAttributes<HTMLElement>) => {
     </section>
   );
 };
+
+const useInit = () =>
+  useCallback(({ set }: MutableSnapshot) => {
+    const size = 2;
+    const coordinates: Array<DRCellId> = [];
+    for (let x = -size; x <= size; x++) {
+      for (let y = -size; y <= size; y++) {
+        coordinates.push(toDRCellId(x, y));
+      }
+    }
+    set(rcAddCells, coordinates);
+  }, []);
