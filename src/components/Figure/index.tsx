32f7e53153a02b4afad676595c165e86b7bd1f84
Kei Ito <kei.itof@gmail.com>
2024-10-16 01:02:05 +0900

    feat: Figureにtypeを追加

--- src/components/Figure/index.tsx
+++ src/components/Figure/index.tsx
@@ -2,15 +2,17 @@ import type { PropsWithChildren, ReactNode } from "react";
 
 interface FigureProps {
 	id: string;
+	type: string;
 	caption?: ReactNode;
 }
 
 export const Figure = ({
 	id,
+	type,
 	caption,
 	children,
 }: PropsWithChildren<FigureProps>) => (
-	<figure className={caption ? "caption" : undefined}>
+	<figure className={caption ? "caption" : undefined} data-type={type}>
 		<span id={id} className="fragment-target" />
 		<figcaption>
 			<span>{caption}</span>
