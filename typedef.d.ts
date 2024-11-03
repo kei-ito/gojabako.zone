d9df8308b2ef24c9b93bc2d2b4744802788c1934
Kei Ito <kei.itof@gmail.com>
2024-11-03 16:57:37 +0900

    deps: @svgr/webpackを追加

--- typedef.d.ts
+++ typedef.d.ts
@@ -14,3 +14,14 @@ declare module "@storybook/react" {
 		? Story<P>
 		: never;
 }
+
+declare module "*.svg" {
+	import type { FC, SVGProps } from "react";
+	const content: FC<SVGProps<SVGElement>>;
+	export default content;
+}
+
+declare module "*.svg?url" {
+	const content: unknown;
+	export default content;
+}
