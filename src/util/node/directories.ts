04287fb0190cbe25c10e8d94f8696ed74836ab85
Kei Ito <kei.itof@gmail.com>
2024-11-03 12:52:36 +0900

    ci: brands.ts生成スクリプトを追加

--- src/util/node/directories.ts
+++ src/util/node/directories.ts
@@ -3,3 +3,4 @@ export const srcDir = new URL("src/", rootDir);
 export const cacheDir = new URL(".cache/", rootDir);
 export const appDir = new URL("app/", srcDir);
 export const componentsDir = new URL("components/", srcDir);
+export const brandIconDir = new URL("public/fa-6.6.0/brands/", rootDir);
