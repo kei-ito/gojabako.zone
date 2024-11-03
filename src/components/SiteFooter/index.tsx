0d816d3e126e79feb4a142c03c26f20f6adb86d5
Kei Ito <kei.itof@gmail.com>
2024-11-03 16:58:24 +0900

    feat: URL修正、フッターにリンクを追加

--- src/components/SiteFooter/index.tsx
+++ src/components/SiteFooter/index.tsx
@@ -1,18 +1,34 @@
 import Link from "next/link";
 import type { HTMLAttributes } from "react";
+import IconGitHub from "../../svg/fa-6.6.0/brands/github.svg";
+import IconXTwitter from "../../svg/fa-6.6.0/brands/x-twitter.svg";
+import IconRss from "../../svg/rss.svg";
 import { classnames } from "../../util/classnames.ts";
 import { site } from "../../util/site.ts";
 import { SiteMap } from "../SiteMap";
 import * as style from "./style.module.scss";
+
+const links = [
+	[IconRss, "/feed.atom"],
+	[IconGitHub, "https://github.com/gjbkz"],
+	[IconXTwitter, "https://x.com/gjbkz"],
+];
 
 export const SiteFooter = (props: HTMLAttributes<HTMLElement>) => (
 	<footer {...props} className={classnames(style.container, props.className)}>
 		<section>
 			<SiteMap />
 		</section>
-		<section>
+		<section className={style.author}>
 			<div>
 				© 2013- <Link href="/author">{site.author.name}</Link>
+				<div className={style.links}>
+					{links.map(([Icon, href]) => (
+						<Link key={href} href={href} target="_blank">
+							<Icon />
+						</Link>
+					))}
+				</div>
 			</div>
 		</section>
 	</footer>
