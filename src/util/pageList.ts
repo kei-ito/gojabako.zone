54762d520dbcabe6c8530fdfe7b65db2328ad129
Kei Ito <kei.itof@gmail.com>
2024-11-04 15:34:02 +0900

    chore: pageList更新

--- src/util/pageList.ts
+++ src/util/pageList.ts
@@ -37,8 +37,8 @@ export const pageList: Array<PageData> = [
 	{
 		title: ["塗られた", "マス目の", "境界を", "描く"],
 		publishedAt: "2024-11-02T17:15:58.000Z",
-		updatedAt: "2024-11-04T03:04:00.000Z",
-		commits: 9,
+		updatedAt: "2024-11-04T04:56:21.000Z",
+		commits: 11,
 		path: "/2024/boundary-tracing",
 		iri: "gjbkz://gojabako.zone/2024/boundary-tracing",
 		group: "2024",
@@ -260,8 +260,8 @@ export const pageList: Array<PageData> = [
 	},
 	{
 		publishedAt: "2023-09-18T10:16:16.000Z",
-		updatedAt: "2024-11-04T03:04:00.000Z",
-		commits: 79,
+		updatedAt: "2024-11-04T04:56:21.000Z",
+		commits: 81,
 		title: ["Home"],
 		path: "/",
 		iri: "gjbkz://gojabako.zone/",
