5840e4ab43232912be505f970b9b3b70bb32631a
Kei Ito <kei.itof@gmail.com>
2024-11-17 22:48:00 +0900

    chore: update pageList.ts

--- src/util/pageList.ts
+++ src/util/pageList.ts
@@ -266,8 +266,8 @@ export const pageList: Array<PageData> = [
 	},
 	{
 		publishedAt: "2023-09-18T10:16:16.000Z",
-		updatedAt: "2024-11-17T10:10:15.000Z",
-		commits: 94,
+		updatedAt: "2024-11-17T13:47:51.000Z",
+		commits: 93,
 		title: ["Home"],
 		path: "/",
 		iri: "gjbkz://gojabako.zone/",
