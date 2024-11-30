7eb776ab86974d75e970f850766a13a6eff84d5a
Kei Ito <kei.itof@gmail.com>
2024-11-30 18:03:24 +0900

    fix: delete d-reversi

--- src/util/recoil/selector.ts
+++ src/util/recoil/selector.ts
@@ -1,17 +1,33 @@
-import type { CallbackInterface, RecoilState, RecoilValue } from "recoil";
+import type {
+	AtomAdapter,
+	Snapshot,
+	UseStateCallbackParams,
+} from "jotai-recoil-adapter";
 
 export interface RecoilSelectorOpts {
-	set: <T>(recoilVal: RecoilState<T>, newVal: T | ((currVal: T) => T)) => void;
-	get: <T>(recoilVal: RecoilValue<T>) => T;
-	reset: <T>(recoilVal: RecoilState<T>) => void;
+	set: <T>(recoilVal: AtomAdapter<T>, newVal: T | ((currVal: T) => T)) => void;
+	get: <T>(recoilVal: AtomAdapter<T>) => T;
+	reset: <T>(recoilVal: AtomAdapter<T>) => void;
 }
+
+export const getLoadableValue = <T>(
+	snapshot: Snapshot,
+	atom: AtomAdapter<T>,
+): T => {
+	const loadable = snapshot.getLoadable(atom);
+	if (loadable.state === "hasValue") {
+		return loadable.contents;
+	}
+	throw new Error(
+		`The loadable state is not 'hasValue' but '${loadable.state}'`,
+	);
+};
 
 export const toRecoilSelectorOpts = ({
 	set,
 	reset,
 	snapshot,
-}: CallbackInterface): RecoilSelectorOpts => {
-	const get = <T>(recoilValue: RecoilValue<T>) =>
-		snapshot.getLoadable(recoilValue).getValue();
+}: UseStateCallbackParams): RecoilSelectorOpts => {
+	const get = <T>(atom: AtomAdapter<T>) => getLoadableValue(snapshot, atom);
 	return { set, reset, get };
 };
