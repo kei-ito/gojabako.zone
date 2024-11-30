ad148192a9e774efce9680f780761a533b14ea6f
Kei Ito <kei.itof@gmail.com>
2024-11-30 18:36:12 +0900

    feat: add app-host header

--- src/util/env.ts
+++ src/util/env.ts
@@ -1,4 +1,21 @@
 export const isServer = typeof window === "undefined";
+export const isClient = !isServer;
 export const isNode =
 	typeof process !== "undefined" && Boolean(process.version);
-export const isClient = !isServer;
+export const env =
+	(typeof process !== "undefined" && process.env) || ({} as NodeJS.ProcessEnv);
+export const appHost = (() => {
+	if (env.VERCEL) {
+		return "vercel" as const;
+	}
+	if (env.NETLIFY) {
+		return "netlify" as const;
+	}
+	if (env.K_SERVICE) {
+		return "gcp" as const;
+	}
+	if (env.AWS_APP_ID) {
+		return "aws" as const;
+	}
+	return "local" as const;
+})();
