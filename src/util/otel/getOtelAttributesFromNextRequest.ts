339fd1d470505fb71592791dccf80a25dfc4488e
Kei Ito <kei.itof@gmail.com>
2024-11-30 13:53:40 +0900

    deps: next:14.2.15→15.0.3

--- src/util/otel/getOtelAttributesFromNextRequest.ts
+++ src/util/otel/getOtelAttributesFromNextRequest.ts
@@ -1,5 +1,4 @@
 import {
-	ATTR_CLIENT_ADDRESS,
 	ATTR_HTTP_REQUEST_METHOD,
 	ATTR_SERVER_ADDRESS,
 	ATTR_SERVER_PORT,
@@ -14,11 +13,9 @@ import { site } from "../site";
 const NA = "n/a";
 const ATTR_APP = (key: string) => `${site.namespace}.${key}`;
 const ATTR_APP_REQ = (key: string) => ATTR_APP(`req.${key}`);
-const ATTR_APP_REQ_GEO = (key: string) => ATTR_APP_REQ(`geo.${key}`);
 
 export const getOtelAttributesFromNextRequest = (request: NextRequest) => {
 	const attributes: Record<string, string> = {
-		[ATTR_CLIENT_ADDRESS]: request.ip ?? NA,
 		[ATTR_HTTP_REQUEST_METHOD]: request.method,
 		[ATTR_SERVER_ADDRESS]: request.nextUrl.hostname,
 		[ATTR_SERVER_PORT]: request.nextUrl.port ?? NA,
@@ -30,10 +27,5 @@ export const getOtelAttributesFromNextRequest = (request: NextRequest) => {
 		[ATTR_APP_REQ("referer")]: request.referrer ?? NA,
 		[ATTR_APP_REQ("user_agent")]: request.headers.get("user-agent") ?? NA,
 	};
-	if (request.geo) {
-		for (const [key, value] of Object.entries(request.geo)) {
-			attributes[ATTR_APP_REQ_GEO(key)] = value;
-		}
-	}
 	return attributes;
 };
