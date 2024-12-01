2f3b44c9f6a0b383b4092ffa152b61c5d3f4b15f
Kei Ito <kei.itof@gmail.com>
2024-12-01 15:29:42 +0900

    fix: attribute names

--- src/util/otel/otelResource.ts
+++ src/util/otel/otelResource.ts
@@ -11,26 +11,27 @@ import { appVersion } from "../version";
 const listRuntimeAttributes = function* (): Generator<
 	[string, string | undefined]
 > {
+	const ns = site.namespace;
 	yield [ATTR_SERVICE_NAME, site.name];
 	yield [ATTR_SERVICE_VERSION, appVersion];
-	yield ["app_host", appHost];
-	yield ["node_env", env.NODE_ENV];
-	yield ["node_version", process.version];
-	yield ["vercel.env", env.VERCEL_ENV];
-	yield ["vercel.edge", env.VERCEL_EDGE];
-	yield ["vercel.region", env.VERCEL_REGION];
-	yield ["vercel.deployment_id", env.VERCEL_DEPLOYMENT_ID];
-	yield ["netlify.build_id", env.BUILD_ID];
-	yield ["netlify.context", env.CONTEXT];
-	yield ["netlify.deploy_id", env.DEPLOY_ID];
-	yield ["gcp.service", env.K_SERVICE];
-	yield ["gcp.revision", env.K_REVISION];
-	yield ["gcp.configuration", env.K_CONFIGURATION];
-	yield ["gcp.cloud_run_job", env.CLOUD_RUN_JOB];
-	yield ["gcp.cloud_run_execution", env.CLOUD_RUN_EXECUTION];
-	yield ["aws.app_id", env.AWS_APP_ID];
-	yield ["aws.region", env.AWS_REGION];
-	yield ["aws.execution_env", env.AWS_EXECUTION_ENV];
+	yield [`${ns}.app_host`, appHost];
+	yield [`${ns}.node_env`, env.NODE_ENV || env.VERCEL_ENV];
+	yield [`${ns}.node_version`, process.version];
+	yield [`${ns}.vercel.env`, env.VERCEL_ENV];
+	yield [`${ns}.vercel.edge`, env.VERCEL_EDGE];
+	yield [`${ns}.vercel.region`, env.VERCEL_REGION];
+	yield [`${ns}.vercel.deployment_id`, env.VERCEL_DEPLOYMENT_ID];
+	yield [`${ns}.netlify.build_id`, env.BUILD_ID];
+	yield [`${ns}.netlify.context`, env.CONTEXT];
+	yield [`${ns}.netlify.deploy_id`, env.DEPLOY_ID];
+	yield [`${ns}.gcp.service`, env.K_SERVICE];
+	yield [`${ns}.gcp.revision`, env.K_REVISION];
+	yield [`${ns}.gcp.configuration`, env.K_CONFIGURATION];
+	yield [`${ns}.gcp.cloud_run_job`, env.CLOUD_RUN_JOB];
+	yield [`${ns}.gcp.cloud_run_execution`, env.CLOUD_RUN_EXECUTION];
+	yield [`${ns}.aws.app_id`, env.AWS_APP_ID];
+	yield [`${ns}.aws.region`, env.AWS_REGION];
+	yield [`${ns}.aws.execution_env`, env.AWS_EXECUTION_ENV];
 };
 
 export const otelResource = new Resource(
@@ -38,7 +39,7 @@ export const otelResource = new Resource(
 		const attributes: Record<string, string> = {};
 		for (const [key, value] of listRuntimeAttributes()) {
 			if (isString(value)) {
-				attributes[`${site.namespace}.${key}`] = value;
+				attributes[key] = value;
 			}
 		}
 		return attributes;
