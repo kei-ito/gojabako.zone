8a4f8c3cd643ea4d99fb4b84315180a79d107dc1
Kei Ito <kei.itof@gmail.com>
2024-12-15 22:41:09 +0900

    fix: NEXT_PUBLIC_APP_HOSTでappHostを取得するよう修正

--- src/util/env.ts
+++ src/util/env.ts
@@ -2,23 +2,4 @@ export const hasProcess = typeof process === "object";
 export const hasWindow = typeof window === "object";
 export const hasLocation = typeof location === "object";
 export const isNode = hasProcess && Boolean(process.version);
-export const appHost = (() => {
-	if (hasProcess) {
-		if (process.env.APP_HOST) {
-			return process.env.APP_HOST;
-		}
-		if (process.env.VERCEL) {
-			return "Vercel";
-		}
-		if (process.env.NETLIFY) {
-			return "Netlify";
-		}
-		if (process.env.K_SERVICE) {
-			return "GCP";
-		}
-		if (process.env.AWS_REGION) {
-			return "AWS";
-		}
-	}
-	return null;
-})();
+export const appHost = process.env.NEXT_PUBLIC_APP_HOST;
